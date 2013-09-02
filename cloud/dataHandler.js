f7b4f6d735caba1154155c4e6d7d988bc4bab725
nsharma@gmail.com <nsharma@gmail.com>
2013-09-02 10:54:31 +0000

    update file /cloud/dataHandler.js

--- cloud/dataHandler.js
+++ cloud/dataHandler.js
@@ -1,10 +1,32 @@
 var collisions = {};
 
 exports.doList = function(dataset_id, params, cb) {
+  console.log("doList : ", dataset_id, " :: ", params);
 
-  return cb(null, {
-      "uid": "1",
-      "name": "nitesh"
+  $fh.db({
+    "act": "list",
+    "type": dataset_id
+  }, function(err, res) {
+    console.log( "res", res)
+    if (err) return cb(err);
+
+    var resJson = {};
+
+    for (var di = 0, dl = res.list.length; di < dl; di += 1) {
+      resJson[res.list[di].guid] = res.list[di].fields;
+    }
+
+    if( params && params.syncDelay && !isNaN(params.syncDelay) ) {
+      // Simulate a delay with list operation
+      setTimeout(function() {
+        console.log("if", resJson)
+        return cb(null, resJson);
+      }, (params.syncDelay * 1000))
+    }
+    else {
+      console.log("if", resJson)
+      return cb(null, resJson);
+    }
   });
 };
 
