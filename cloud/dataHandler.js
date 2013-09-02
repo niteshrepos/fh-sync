85a0d8fa71d9a08f67ec6a3b41889ac8b50e8744
nsharma@gmail.com <nsharma@gmail.com>
2013-09-02 11:10:59 +0000

    update file /cloud/dataHandler.js

--- cloud/dataHandler.js
+++ cloud/dataHandler.js
@@ -1,5 +1,5 @@
 var collisions = {};
 
 exports.doList = function(dataset_id, params, cb) {
- return { '522468ffdd75647966cb992c': { created: 1378117886091, name: 'some text 2' } }
+   return cb(null, { '522468ffdd75647966cb992c': { created: 1378117886091, name: 'some text 2' } });
 };
